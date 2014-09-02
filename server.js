a0af1f06b55c8cd5f48bd4285046ab54eecd6afc
kirtithorat <kirti.brenz@gmail.com>
2014-09-02 18:16:33 -0500

    Added specific status messages

--- server.js
+++ server.js
@@ -1,40 +1,51 @@
 var mongo = require('mongodb').MongoClient,
-  client = require('socket.io').listen(8080).sockets;
+    client = require('socket.io').listen(8080).sockets;
 
 mongo.connect('mongodb://127.0.0.1/chat', function(err, db) {
-  if (err) throw err;
-  client.on('connection', function(socket) {
-    var col = db.collection('messages'),
-        sendStatus = function (s) {
-          socket.emit('status', s);
-        };
+    if (err) throw err;
+    client.on('connection', function(socket) {
+        var col = db.collection('messages'),
+            sendStatus = function(s) {
+                socket.emit('status', s);
+            };
 
-    // Emit all messages
-    col.find().limit(100).sort({_id: 1}).toArray(function (err, res) {
-      if(err) throw err;
-      socket.emit('output', res);
+        // Emit all messages
+        col.find().limit(100).sort({
+            _id: 1
+        }).toArray(function(err, res) {
+            if (err) throw err;
+            socket.emit('output', res);
+        });
+
+        //Wait for input
+        socket.on('input', function(data) {
+            var name = data.name,
+                message = data.message,
+                whitespacePattern = /^\s*$/,
+                isNameValid = whitespacePattern.test(name),
+                isMessageValid = whitespacePattern.test(message);
+
+            if (isNameValid && isMessageValid) {
+                sendStatus("You forgot to enter name and message in chat.");
+            } else if (isNameValid) {
+                sendStatus("Did you forget to enter your name?");
+            } else if (isMessageValid) {
+                sendStatus("Add a message to chat.");
+            } else {
+                col.insert({
+                    name: name,
+                    message: message
+                }, function() {
+                    // Emit latest message to All clients
+                    client.emit('output', [data]);
+
+                    sendStatus({
+                        message: "Message Sent",
+                        clear: true
+                    });
+                });
+            }
+
+        });
     });
-
-    //Wait for input
-    socket.on('input', function(data) {
-      var name = data.name,
-          message = data.message,
-          whitespacePattern = /^\s*$/;
-
-      if(whitespacePattern.test(name) || whitespacePattern.test(message)) {
-        sendStatus("Name and message is required.");
-      } else {
-        col.insert({name: name, message: message}, function() {
-          // Emit latest message to All clients
-          client.emit('output', [data]);
-
-          sendStatus({
-            message: "Message Sent", 
-            clear: true
-          });
-        });       
-      }   
-
-    });
-  });
 });
